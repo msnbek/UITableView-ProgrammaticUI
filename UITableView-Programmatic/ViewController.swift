759db991a6b3afe444ddc381d623a348af88eb59
msnbek <mahmutsenbek@icloud.com>
2023-01-25 10:37:59 +0300

    Initial Commit

---
+++ UITableView-Programmatic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableView-Programmatic
+//
+//  Created by Mahmut Senbek on 25.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
